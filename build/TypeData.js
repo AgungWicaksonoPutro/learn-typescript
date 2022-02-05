bbe49acaf53860e2c296e8a0a58d0ade0919be71
Agung Wicaksono Putro <C090821003@Supporting.corp.bankmandiri.co.id>
2022-02-05 21:41:02 +0700

    Data Type

---
+++ build/TypeData.js
@@ -0,0 +1,12 @@
+"use strict";
+// String Data Type
+let fullName = "John Doe";
+// Number Data Type 
+let age = 12;
+// Boolean Data Type
+let isMarried = true;
+// Any Data Type
+let multy = "John Doe";
+multy = true;
+multy = 12;
+multy = "John" || true || 12;
